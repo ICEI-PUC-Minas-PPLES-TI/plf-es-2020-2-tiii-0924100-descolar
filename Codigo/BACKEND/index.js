4a06f4fa73ba027166c4964cffda324a92327d77
Júlia Prado <juliafpra@gmail.com>
2020-11-21 23:22:50 -0300

    Configurações da API de cadastro

---
+++ Codigo/BACKEND/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const { Client } = require('pg')
+
+const client = new Client()
+client.connect()
+const app = express()
+const port = process.env.PORT || 3000
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/usuarios', async (req, res) => {
+    const clientes = await client.query('SELECT *FROM cliente');
+    res.header("content-type", "application/json")
+    res.send(JSON.stringify(clientes.rows, null, 2))
+})
+
+app.post('/cadastro', async (req, res) => {
+    try {
+        await client.query('INSERT INTO cliente (nome, email, senha, cpf, cnpj) VALUES ($1,$2,$3,$4,$5)',
+            [req.body.nome, req.body.email, req.body.senha, req.body.cpf, req.body.cnpj]);
+        res.send('OK!')
+
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
