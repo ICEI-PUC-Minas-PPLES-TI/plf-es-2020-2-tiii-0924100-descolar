0ac2b9bcbdb875c7313268c87ce8ac06e7341d61
Júlia Prado <juliafpra@gmail.com>
2020-11-22 20:20:44 -0300

    Adicionar o CORS

--- Codigo/BACKEND/index.js
+++ Codigo/BACKEND/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
 const { Client } = require('pg')
@@ -9,6 +10,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 
 app.get('/', (req, res) => {
@@ -25,10 +27,11 @@ app.post('/cadastro', async (req, res) => {
     try {
         await client.query('INSERT INTO cliente (nome, email, senha, cpf, cnpj) VALUES ($1,$2,$3,$4,$5)',
             [req.body.nome, req.body.email, req.body.senha, req.body.cpf, req.body.cnpj]);
-        res.send('OK!')
+            res.send('OK!')
 
     } catch (error) {
         res.status(500)
+        console.error(error.message)
         res.send(error.message)
     }
 })
