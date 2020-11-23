70b1129416d226eecd65b0218490f2c96d811b31
Júlia Prado <juliafpra@gmail.com>
2020-11-22 21:22:15 -0300

    API de Login

--- Codigo/BACKEND/index.js
+++ Codigo/BACKEND/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
 const { Client } = require('pg')
+const { v4: gerarID } = require("uuid")
 
 const client = new Client()
 client.connect()
@@ -24,7 +25,29 @@ app.post('/cadastro', async (req, res) => {
     try {
         await client.query('INSERT INTO cliente (nome, email, senha, cpf, cnpj) VALUES ($1,$2,$3,$4,$5)',
             [req.body.nome, req.body.email, req.body.senha, req.body.cpf, req.body.cnpj]);
-            res.send('OK!')
+        res.send('OK!')
+
+    } catch (error) {
+        res.status(500)
+        console.error(error.message)
+        res.send(error.message)
+    }
+})
+
+app.post('/entrar', async (req, res) => {
+    try {
+        const resultado = await client.query('SELECT* FROM cliente WHERE email = $1 AND senha = $2',
+            [req.body.email, req.body.senha])
+        if (resultado.rowCount == 0) {
+            res.status(404)
+            res.send("not found")
+        }
+        else {
+            const token = gerarID()
+            await client.query('INSERT INTO token (token, cod_cliente) VALUES ($1,$2)',
+                [token, resultado.rows[0].cod_cliente])
+            res.send(token)
+        }
 
     } catch (error) {
         res.status(500)
